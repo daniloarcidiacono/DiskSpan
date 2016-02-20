b15a8f5e29e79c345170f3788c798f276a385000
Danilo Arcidiacono <danilo.arcidiacono@gmail.com>
2016-02-20 21:06:29 +0100

    Fixed FSUtils::formatSize() bug in constants.

--- src/FSUtils.h
+++ src/FSUtils.h
@@ -34,8 +34,8 @@ public:
 	static QString formatSize(const quint64 sizeInBytes)
 	{
 		const quint64 KB = 1024;
-		const quint64 MB = KB * KB;
-		const quint64 GB = MB * MB;
+		const quint64 MB = 1024 * KB;
+		const quint64 GB = 1024 * MB;
 
 		if (sizeInBytes < KB)
 			return QString("%1 bytes").arg(sizeInBytes);
