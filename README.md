bbc368cd5a60b70fea7a7990ca2d26abd28ee8e0
Danilo Arcidiacono <danilo.arcidiacono@gmail.com>
2016-02-21 12:49:19 +0100

    Added LGPL license. Added quit icon.

--- README.md
+++ README.md
@@ -23,3 +23,7 @@ sudo apt-get install mesa-common-dev
 
 ### /usr/bin/ld: cannot find -lGL
 sudo apt-get install libglu1-mesa-dev
+
+## License
+
+[GNU LGPL license](https://www.openclinica.com/gnu-lgpl-open-source-license)
--- src/ItemDetailsModel.cpp
+++ src/ItemDetailsModel.cpp
@@ -1,3 +1,19 @@
+/*  Copyright 2016 Danilo Arcidiacono. All Rights Reserved.
+ *  This file is part of DiskSpan.
+ *
+ *  DiskSpan is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  DiskSpan is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with DiskSpan.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "ItemDetailsModel.h"
 #include "FSUtils.h"
 #include <QDebug>
